65b0c099e120740c24cf83bc74f4b5601cf17d88
anikamorris <anikamorris11@gmail.com>
2019-11-17 22:08:59 -0800

    fixed git remote origin

---
+++ sushi-neko/Character.swift
@@ -0,0 +1,39 @@
+//
+//  Character.swift
+//  sushi-neko
+//
+//  Created by Anika Morris on 11/15/19.
+//  Copyright © 2019 Anika Morris. All rights reserved.
+//
+
+import SpriteKit
+
+class Character: SKSpriteNode {
+    
+    let punch = SKAction(named: "Punch")!
+
+    /* Character side */
+    var side: Side = .left {
+        didSet {
+            if side == .left {
+                xScale = 1
+                position.x = 70
+            } else {
+                /* An easy way to flip an asset horizontally is to invert the X-axis scale */
+                xScale = -1
+                position.x = 252
+            }
+            run(punch)
+        }
+    }
+    
+    /* You are required to implement this for your subclass to work */
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
